7519afe29fd45b39b2572f3d26644e9a62a5ce43
Adrian R <arodriguezpovea@gmail.com>
2019-04-01 13:10:04 +0200

    Añadiendo menu movies.py

---
+++ movies.py
@@ -0,0 +1,29 @@
+
+
+
+
+while True:
+	print ('''
+	Elige una opcion:
+	1-Lista el título, año y duración de todas las películas.
+	2-Muestra los títulos de las películas y el número de actores/actrices que tiene cada una.
+	3-Muestra las películas que contengan en la sinopsis dos palabras dadas.
+	4-Muestra las películas en las que ha trabajado un actor dado.
+	5-Muestra el título y la url del póster de las tres películas con una media de puntuaciones más alta y lanzadas entre dos fechas dadas.
+	0-Para salir.
+	''')
+	opcion=int(input("Opcion: "))
+
+	if opcion==1:
+		
+	elif opcion==2:
+		
+	elif opcion==3:
+		
+	elif opcion==4:
+
+	elif opcion==5:
+	
+	elif opcion==0:
+		print("Hasta la proxima!")
+		break;
